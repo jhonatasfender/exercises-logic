75e4e8b1c11db8fb387f90f611240ef8c2a44b81
jhonatasfender <jhonatas.fender@gmail.com>
2017-06-22 16:47:15 -0300

    exercicio de array

--- 2 - Arrays/exercise01/JavaScript/main.js
+++ 2 - Arrays/exercise01/JavaScript/main.js
@@ -1,26 +1,22 @@
+/*
+[55%] Your score on this task OddOccurrencesInArray
+First try
+*/
 
-function solution(A, K) {
-    // write your code in JavaScript (Node.js 4.0.0)
-    /**
-     * [3,8,9,7,6]
-     * [6,3,8,9,7]
-     * [7,6,3,8,9]
-     * [9,7,6,3,8]
-     * 
-     */
-    for (var i = 0; i <= K; i++) {
-        var c, b, d = 0;
-        for (var j = 0; j < A.length; j++) {
-            if (true) {
-                
-            } else {
+function solutionFirstTry(A) {
+    let d = [];
+    A.filter(function(b, i, j) {
+        for (let c = 0; c < A.length; c++) {
+            if (i != c && A[c] == b) {
+                d[i] = true;
             }
         }
-    }
-    console.log(A);
-    //return 0;
+        return true;
+    });
+    for (let c = 0; c < A.length; c++)
+        if (d[c] == undefined)
+            return A[c];
 }
-
-window.onload = function () {
-    solution([3, 8, 9, 7, 6], 3);
+window.onload = function() {
+    console.log(solutionFirstTry([9, 3, 9, 3, 9, 7, 9]));
 };
---
+++ 2 - Arrays/exercise02/JavaScript/main.js
@@ -0,0 +1,26 @@
+
+function solution(A, K) {
+    // write your code in JavaScript (Node.js 4.0.0)
+    /**
+     * [3,8,9,7,6]
+     * [6,3,8,9,7]
+     * [7,6,3,8,9]
+     * [9,7,6,3,8]
+     * 
+     */
+    for (var i = 0; i <= K; i++) {
+        var c, b, d = 0;
+        for (var j = 0; j < A.length; j++) {
+            if (true) {
+                
+            } else {
+            }
+        }
+    }
+    console.log(A);
+    //return 0;
+}
+
+window.onload = function () {
+    solution([3, 8, 9, 7, 6], 3);
+};
